7bf0335115e8a802baf7ce67018c4f6786e5a6a6
KillerBe14 <ronronz145@gmail.com>
2019-04-03 15:10:47 +0300

    4th commit fixed BreadthFS

--- src/algorithms/search/BreadthFirstSearch.java
+++ src/algorithms/search/BreadthFirstSearch.java
@@ -10,4 +10,9 @@ public class BreadthFirstSearch extends ASearchingAlgorithm {
     public String getName() {
         return null;
     }
+
+    @Override
+    public int getNumberOfNodesEvaluated() {
+        return 0;
+    }
 }
