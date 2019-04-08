753dcca521ed2b3de5e23f01d3d8b9dbd6cc3511
yuvalkh <45626643+yuvalkh@users.noreply.github.com>
2019-04-09 00:41:14 +0300

    Searching Algorithms Work ! Maze Generators Work ! JavaCode and JUnit is finished ! Formalization Finished ! Replaced LinkedList with Hash to be able to solve 1000x1000 Fixed some things in JUint and added generate star and end points in create maze Fixed the infinite loop in 2x2 and more on mazeGenerator Fixed the BFS and the BestFS and the DFS

--- src/algorithms/mazeGenerators/Maze.java
+++ src/algorithms/mazeGenerators/Maze.java
@@ -112,6 +112,7 @@ public class Maze {
     /**
      * prints maze with solutions
      */
+
     public void print() {
         for (int i = 0; i < numOfRows; i++) {
             for (int j = 0; j < numOfColumns; j++) {
@@ -125,7 +126,8 @@ public class Maze {
             System.out.println();
         }
     }
-    /*
+
+/*
     public void print() {
         for (int i = 0; i < numOfRows; i++) {
             for (int j = 0; j < numOfColumns; j++) {
@@ -146,7 +148,7 @@ public class Maze {
             System.out.println(" " + "\u001B[107m");
         }
     }
-    */
+*/
     /**
      * generates random start Cell and Goal Cell
      */
--- src/algorithms/search/DepthFirstSearch.java
+++ src/algorithms/search/DepthFirstSearch.java
@@ -17,18 +17,19 @@ public class DepthFirstSearch extends ASearchingAlgorithm {
             AState temp = goal;
             Stack<AState> stack = new Stack<>();
             stack.push(goal);
+            discovered.add(goal);
             while (!stack.isEmpty()) {
                 temp = stack.pop();
                 if (s.getStartState().equals(temp)) {
                     break;
                 }
-                if (!discovered.contains(temp)) {
-                    discovered.add(temp);
                     for (AState adjState : s.getAllSuccessors(temp)) {
-                        stack.push(adjState);
-                        NumberOfNodesEvaluated++;
+                        if(!discovered.contains(adjState)){
+                            discovered.add(adjState);
+                            stack.push(adjState);
+                            NumberOfNodesEvaluated++;
+                        }
                     }
-                }
             }
             for (AState state = temp; state != null; state = state.getCameFrom()) {
                 sol.addState(state);
