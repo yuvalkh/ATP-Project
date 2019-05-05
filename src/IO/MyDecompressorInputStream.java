1d8b4c2aa0c3ec6ceb9755a2cd43a1d4a60c7161
yuvalkh <45626643+yuvalkh@users.noreply.github.com>
2019-05-06 00:19:35 +0300

    Part A Done ! Finished write function on MyCompressor/MyDecompressor and finish to save and upload

--- src/IO/MyDecompressorInputStream.java
+++ src/IO/MyDecompressorInputStream.java
@@ -5,12 +5,40 @@ import java.io.InputStream;
 
 public class MyDecompressorInputStream extends InputStream {
     InputStream in;
+
     public MyDecompressorInputStream(InputStream inputstream) {
         in = inputstream;
     }
 
+
     @Override
     public int read() throws IOException {
+
         return 0;
+    }
+
+    @Override
+    public int read(byte[] b) throws IOException {
+        int numOfRead = 0;
+        int index = 12;
+        byte oneORzero = 0;
+        byte[] Compressed = new byte[in.available()];
+        in.read(Compressed);//puts inside it the compressed array
+        for (int i = 0; i < 12; i++) {//first, we put all the properties
+            b[i] = Compressed[i];
+            numOfRead++;
+        }
+        for (int i = 12; i <Compressed.length; i++) {//second, we put the maze
+            for (int j = 0; j < Compressed[i]; j++) {
+                b[index] = oneORzero;
+                index++;
+                numOfRead++;
+            }
+            if(oneORzero == 1)
+                oneORzero = 0;
+            else
+                oneORzero = 1;
+        }
+        return numOfRead;
     }
 }
