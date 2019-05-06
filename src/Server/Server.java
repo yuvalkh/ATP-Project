65468daf543a9e80ac7435398a2d9a20a5268f8b
yuvalkh <45626643+yuvalkh@users.noreply.github.com>
2019-05-06 14:19:14 +0300

    Part A Done ! Finished Compress and Decompress. Finished writing server strategies.

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -14,7 +14,6 @@ public class Server {
     private int listeningInterval;
     private IServerStrategy serverStrategy;
     private volatile boolean stop;
-    //private static final Logger LOG = LogManager.getLogger(); //Log4j2
 
     public Server(int port, int listeningInterval, IServerStrategy serverStrategy) {
         this.port = port;
@@ -32,39 +31,31 @@ public class Server {
         try {
             ServerSocket serverSocket = new ServerSocket(port);
             serverSocket.setSoTimeout(listeningInterval);
-            //LOG.info(String.format("Server starter at %s!", serverSocket));
-            //LOG.info(String.format("Server's Strategy: %s", serverStrategy.getClass().getSimpleName()));
-            //LOG.info("Server is waiting for clients...");
             while (!stop) {
                 try {
                     Socket clientSocket = serverSocket.accept(); // blocking call
-                    //LOG.info(String.format("Client excepted: %s", clientSocket));
                     new Thread(() -> {
                         handleClient(clientSocket);
-                        //LOG.info(String.format("Finished handle client: %s", clientSocket));
                     }).start();
                 } catch (SocketTimeoutException e) {
-                    //LOG.debug("Socket Timeout - No clients pending!");
                 }
             }
             serverSocket.close();
         } catch (IOException e) {
-            //LOG.error("IOException", e);
+
         }
     }
 
     private void handleClient(Socket clientSocket) {
         try {
-            //LOG.info(String.format("Handling client with socket: %s", clientSocket.toString()));
             serverStrategy.serverStrategy(clientSocket.getInputStream(), clientSocket.getOutputStream());
             clientSocket.close();
         } catch (IOException e) {
-            //LOG.error("IOException", e);
+
         }
     }
 
     public void stop() {
-        //LOG.info("Stopping server..");
         stop = true;
     }
 }
