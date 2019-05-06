42d877ff7d1c6c44b8feaf6dca67b5bee757c46f
yuvalkh <45626643+yuvalkh@users.noreply.github.com>
2019-05-06 13:00:38 +0300

    Part A Done ! Finished Compress and Decompress. Finished writing server strategies.

--- src/Server/ServerStrategyGenerateMaze.java
+++ src/Server/ServerStrategyGenerateMaze.java
@@ -1,12 +1,25 @@
 package Server;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import algorithms.mazeGenerators.*;
+import  IO.MyCompressorOutputStream;
+import java.io.*;
 
 public class ServerStrategyGenerateMaze implements IServerStrategy {
 
     @Override
     public void serverStrategy(InputStream inFromClient, OutputStream outToClient) {
-
+        try {
+            ObjectInputStream fromClient = new ObjectInputStream(inFromClient);
+            //place 0 is numOfRows,place 1 is numOfColumns
+            int[] properties = (int[])fromClient.readObject();//we get from the user an array(suppose to be with length 2)
+            IMazeGenerator mazeGenerator = new MyMazeGenerator();
+            Maze maze = mazeGenerator.generate(properties[0], properties[1]);//now we make the maze
+            MyCompressorOutputStream comp = new MyCompressorOutputStream(outToClient);
+            comp.write(maze.toByteArray());//now we compress it and send it to the client
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/Server/ServerStrategySolveSearchProblem.java
+++ src/Server/ServerStrategySolveSearchProblem.java
@@ -1,12 +1,29 @@
 package Server;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import IO.MyCompressorOutputStream;
+import algorithms.mazeGenerators.*;
+import algorithms.search.*;
+
+import java.io.*;
+import java.util.ArrayList;
 
 public class ServerStrategySolveSearchProblem implements IServerStrategy {
 
     @Override
     public void serverStrategy(InputStream inFromClient, OutputStream outToClient) {
-
+        try {
+            ObjectInputStream fromClient = new ObjectInputStream(inFromClient);
+            ObjectOutputStream toClient = new ObjectOutputStream(outToClient);
+            Maze maze = (Maze)fromClient.readObject();//we get from the user the maze he wants to solve
+            SearchableMaze searchableMaze = new SearchableMaze(maze);//we make that a searchable maze
+            ISearchingAlgorithm searcher = new BestFirstSearch();//we choose with what we want to search
+            Solution solution = searcher.solve(searchableMaze);//we solve the maze
+            toClient.writeObject(solution);//we send to the client the solution
+            toClient.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
